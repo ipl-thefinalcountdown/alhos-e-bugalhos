f2fc9987e51417cce5ffbe7e86a3186e39105b46
Luís Ferreira <contact@lsferreira.net>
2020-10-31 16:18:58 +0000

    netcore: add blank ASP.NET MVC project
    This blank project was generated with `dotnet new mvc` but removed
    the bundled javascript and css libraries. We should build those with
    something like webpack.
    
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace alhos_e_bugalhos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
