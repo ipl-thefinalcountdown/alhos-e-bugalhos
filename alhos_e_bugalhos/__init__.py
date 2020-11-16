b4ed5626b37b137086211c6b6d05f868a809ecd2
Filipe Laíns <lains@riseup.net>
2020-11-16 00:32:40 +0000

    app: add fastapi instance
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- alhos_e_bugalhos/__init__.py
+++ alhos_e_bugalhos/__init__.py
@@ -2,4 +2,17 @@
 alhos_e_bugalhos
 '''
 
+import fastapi
+
+from fastapi.responses import HTMLResponse
+
+
 __version__ = '0.0.0b0'
+
+
+app = fastapi.FastAPI()
+
+
+@app.get('/', response_class=HTMLResponse)
+async def root():
+    return 'Hello!'
