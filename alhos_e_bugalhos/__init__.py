cb9a7ca2c2f0c3cec1e093b0879ca1d85dffe193
Filipe Laíns <lains@riseup.net>
2020-11-30 19:37:04 +0000

    app: add index
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- alhos_e_bugalhos/__init__.py
+++ alhos_e_bugalhos/__init__.py
@@ -2,7 +2,11 @@
 alhos_e_bugalhos
 '''
 
+import os.path
+
 import fastapi
+import mako.lookup
+import mako.template
 
 from fastapi.responses import HTMLResponse
 
@@ -12,7 +16,15 @@ __version__ = '0.0.0b0'
 
 app = fastapi.FastAPI()
 
+templates = mako.lookup.TemplateLookup(directories=[
+    os.path.join(os.path.dirname(__file__), 'templates')
+])
+
+active_connections = []
+
 
 @app.get('/', response_class=HTMLResponse)
 async def root():
-    return 'Hello!'
+    return templates.get_template('index.html').render(
+        connections=active_connections,
+    )
