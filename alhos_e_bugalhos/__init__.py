32329fc75d247a70f4a2a7624fe0a7a18cace02f
Filipe Laíns <lains@riseup.net>
2020-11-30 19:42:30 +0000

    app: add example connections
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- alhos_e_bugalhos/__init__.py
+++ alhos_e_bugalhos/__init__.py
@@ -10,6 +10,9 @@ import mako.template
 
 from fastapi.responses import HTMLResponse
 
+from alhos_e_bugalhos.connections import Connection
+from alhos_e_bugalhos.connections.example import ExampleBackend, ExampleFrontend
+
 
 __version__ = '0.0.0b0'
 
@@ -20,7 +23,38 @@ templates = mako.lookup.TemplateLookup(directories=[
     os.path.join(os.path.dirname(__file__), 'templates')
 ])
 
-active_connections = []
+active_connections = [
+    Connection(
+        'Example 1',
+        ExampleBackend('0.0.0.0', 8080),
+        ExampleFrontend('http://localhost/example1'),
+    ),
+    Connection(
+        'Example 2',
+        ExampleBackend('0.0.0.0', 8081),
+        ExampleFrontend('http://localhost/example2'),
+    ),
+    Connection(
+        'Example 3',
+        ExampleBackend('0.0.0.0', 8082),
+        ExampleFrontend('http://localhost/example3'),
+    ),
+    Connection(
+        'Example 4',
+        ExampleBackend('0.0.0.0', 8083),
+        ExampleFrontend('http://localhost/example4'),
+    ),
+    Connection(
+        'Example 5',
+        ExampleBackend('0.0.0.0', 8084),
+        ExampleFrontend('http://localhost/example5'),
+    ),
+    Connection(
+        'Example 6',
+        ExampleBackend('0.0.0.0', 8086),
+        ExampleFrontend('http://localhost/example6'),
+    ),
+]
 
 
 @app.get('/', response_class=HTMLResponse)
