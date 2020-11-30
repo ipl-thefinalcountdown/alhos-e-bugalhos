014d24dd34e5aca2e057c2a03b199eb0768f23e6
Filipe Laíns <lains@riseup.net>
2020-11-30 19:35:13 +0000

    connections: add base Backend, Frontend and Connection objects
    Signed-off-by: Filipe Laíns <lains@riseup.net>

---
+++ alhos_e_bugalhos/connections/__init__.py
@@ -0,0 +1,45 @@
+import abc
+
+from typing import Dict, Union
+
+
+SettingsDict = Dict[str, Union[str, int]]
+
+
+class Provider(abc.ABC):
+    @property
+    @abc.abstractmethod
+    def type_name(self) -> str:
+        pass
+
+    @property
+    @abc.abstractmethod
+    def settings(self) -> SettingsDict:
+        pass
+
+
+class Backend(Provider):
+    pass
+
+
+class Frontend(Provider):
+    pass
+
+
+class Connection():
+    def __init__(self, name: str, input_: Frontend, output: Backend) -> None:
+        self._name = name
+        self._input = input_
+        self._output = output
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def input(self) -> Frontend:
+        return self._input
+
+    @property
+    def output(self) -> Backend:
+        return self._output
