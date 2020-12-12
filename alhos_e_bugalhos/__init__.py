00c2521c01a7bda6814d0df97b76c4a4f291bccc
Filipe Laíns <lains@riseup.net>
2020-12-12 13:52:13 +0000

    connections: redesign provider model
    This makes it much easier to handle data computionaly. The only drawback
    is making manual construction of Provider classes a bit more painful and
    less Pythonic, but they aren't meant to be constructed like that anyway.
    
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- alhos_e_bugalhos/__init__.py
+++ alhos_e_bugalhos/__init__.py
@@ -11,7 +11,7 @@ import mako.template
 
 from fastapi.responses import HTMLResponse
 
-from alhos_e_bugalhos.connections import Connection, SettingError
+from alhos_e_bugalhos.connections import Backend, Connection, Frontend, SettingError
 from alhos_e_bugalhos.connections.example import ExampleBackend, ExampleFrontend
 
 
@@ -27,33 +27,63 @@ templates = mako.lookup.TemplateLookup(directories=[
 active_connections = [
     Connection(
         'Example 1',
-        ExampleBackend('0.0.0.0', 8080),
-        ExampleFrontend('http://localhost/example1'),
+        ExampleBackend({
+            'Host': '0.0.0.0',
+            'Port': 8081,
+        }),
+        ExampleFrontend({
+            'URL': 'http://localhost/example1',
+        }),
     ),
     Connection(
         'Example 2',
-        ExampleBackend('0.0.0.0', 8081),
-        ExampleFrontend('http://localhost/example2'),
+        ExampleBackend({
+            'Host': '0.0.0.0',
+            'Port': 8082,
+        }),
+        ExampleFrontend({
+            'URL': 'http://localhost/example2',
+        }),
     ),
     Connection(
         'Example 3',
-        ExampleBackend('0.0.0.0', 8082),
-        ExampleFrontend('http://localhost/example3'),
+        ExampleBackend({
+            'Host': '0.0.0.0',
+            'Port': 8083,
+        }),
+        ExampleFrontend({
+            'URL': 'http://localhost/example3',
+        }),
     ),
     Connection(
         'Example 4',
-        ExampleBackend('0.0.0.0', 8083),
-        ExampleFrontend('http://localhost/example4'),
+        ExampleBackend({
+            'Host': '0.0.0.0',
+            'Port': 8084,
+        }),
+        ExampleFrontend({
+            'URL': 'http://localhost/example4',
+        }),
     ),
     Connection(
         'Example 5',
-        ExampleBackend('0.0.0.0', 8084),
-        ExampleFrontend('http://localhost/example5'),
+        ExampleBackend({
+            'Host': '0.0.0.0',
+            'Port': 8085,
+        }),
+        ExampleFrontend({
+            'URL': 'http://localhost/example5',
+        }),
     ),
     Connection(
         'Example 6',
-        ExampleBackend('0.0.0.0', 8086),
-        ExampleFrontend('http://localhost/example6'),
+        ExampleBackend({
+            'Host': '0.0.0.0',
+            'Port': 8086,
+        }),
+        ExampleFrontend({
+            'URL': 'http://localhost/example6',
+        }),
     ),
 ]
 
--- alhos_e_bugalhos/connections/__init__.py
+++ alhos_e_bugalhos/connections/__init__.py
@@ -1,28 +1,64 @@
 import abc
 
-from typing import Dict, Union
+from typing import Any, ClassVar, Dict, List, Optional, Union
 
 
-SettingsDict = Dict[str, Union[str, int]]
+SettingValues = Union[str, int]
+SettingsDict = Dict[str, SettingValues]
 
 
-class SettingError(Exception):
+class ProviderError(Exception):
+    pass
+
+
+class SettingError(ProviderError):
     pass
 
 
 class Provider(abc.ABC):
-    @property
-    @abc.abstractmethod
-    def type_name(self) -> str:
-        pass
+    TYPE_NAME: ClassVar[Optional[str]]
+    SETTINGS: ClassVar[Optional[List[str]]] = None
+
+    def __init__(self, settings) -> None:
+        if not self.TYPE_NAME:
+            raise Provider(
+                f'Error when instantiating {self.__class__.__name__} class: '
+                'missing TYPE_NAME attribute'
+            )
+
+        for setting_name in self.SETTINGS:
+            if ' ' in setting_name:
+                raise SettingError(
+                    f"Invalid setting name, setting names can't contain spaces: {setting_name}"
+                )
+            if setting_name not in settings:
+                raise SettingError(f'Missing setting value: {setting_name}')
+
+        self._settings = {}
+
+        for name, value in settings.items():
+            if name not in self.SETTINGS:
+                raise SettingError(f'Unknown setting: {name}')
+            self.update_setting(name, value)
+
+    def update_setting(self, name: str, value: Any):
+        if name not in self.SETTINGS:
+            raise KeyError(f'Invalid setting: {name}')
+        self._settings[name] = self.validate_setting(name, value)
 
     @property
-    @abc.abstractmethod
     def settings(self) -> SettingsDict:
-        pass
+        return self._settings
+
+    @property
+    def available_settings(self) -> List[str]:
+        return self.SETTINGS
 
     @abc.abstractmethod
-    def update_setting(self, name: str, value: str) -> SettingsDict:
+    def validate_setting(self, name: str, value: Any) -> Optional[SettingValues]:
+        '''
+        Validates a setting value and returns the internal interpretation for it
+        '''
         pass
 
 
