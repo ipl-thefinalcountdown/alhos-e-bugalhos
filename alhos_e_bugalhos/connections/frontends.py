c7102bd0299c4e8691a8fa540546ef4185ec3f56
Filipe Laíns <lains@riseup.net>
2020-12-16 22:19:01 +0000

    frontends: add HTML frontend
    Signed-off-by: Filipe Laíns <lains@riseup.net>

---
+++ alhos_e_bugalhos/connections/frontends.py
@@ -0,0 +1,30 @@
+import uuid
+
+import fastapi
+import json2html
+
+from fastapi.responses import HTMLResponse
+
+import alhos_e_bugalhos
+import alhos_e_bugalhos.connections
+
+
+class HTMLFrontend(alhos_e_bugalhos.connections.Frontend):
+    TYPE_NAME = 'HTML'
+    SETTINGS = {}
+
+    def register(self, get_data):
+        self._id = uuid.uuid4()
+        self._app = fastapi.FastAPI()
+
+        @self._app.get('/', response_class=HTMLResponse)
+        def dispatch():
+            return json2html.json2html.convert(json=get_data())
+
+        self._settings['URL'] = f'@HOST@/{self._id}'
+
+        alhos_e_bugalhos.app.mount(f'/{self._id}', self._app)
+
+    def unresgister(self):
+        # fastapi doesn't let us remove the mount so we replace it
+        alhos_e_bugalhos.app.mount(f'/{self._id}', fastapi.FastAPI())
