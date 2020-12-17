ccda4017c09f7122201b9ac457c47536851a5a63
Filipe Laíns <lains@archlinux.org>
2020-12-17 13:37:13 +0000

    backends: add CSV, JSON and YAML backends

--- alhos_e_bugalhos/connections/backends.py
+++ alhos_e_bugalhos/connections/backends.py
@@ -1,3 +1,6 @@
+import csv
+import io
+import json
 import xml.etree.ElementTree as ET
 
 from typing import Any
@@ -5,6 +8,7 @@ from typing import Any
 import requests
 import validators
 import xmltodict
+import yaml
 
 import alhos_e_bugalhos.connections
 
@@ -27,6 +31,62 @@ class XMLBackend(alhos_e_bugalhos.connections.Backend):
 
     def get_data(self, params=None):
         return xmltodict.parse(self.settings['Data'])
+
+
+class CSVBackend(alhos_e_bugalhos.connections.Backend):
+    TYPE_NAME = 'CSV'
+    SETTINGS = [
+        'Data',
+    ]
+
+    def validate_setting(self, name: str, value: Any):
+        if name == 'Data':
+            if not value:
+                raise alhos_e_bugalhos.connections.SettingError('Invalid Data')
+            try:
+                csv.DictReader(io.StringIO(value))
+            except (ValueError, csv.Error):
+                raise alhos_e_bugalhos.connections.SettingError('Invalid Data')
+            return value
+
+    def get_data(self, params=None):
+        return dict(csv.DictReader(io.StringIO(self.settings['Data'])))
+
+
+class JSONBackend(alhos_e_bugalhos.connections.Backend):
+    TYPE_NAME = 'JSON'
+    SETTINGS = [
+        'Data',
+    ]
+
+    def validate_setting(self, name: str, value: Any):
+        if name == 'Data':
+            try:
+                json.loads(value)
+            except json.JSONDecodeError:
+                raise alhos_e_bugalhos.connections.SettingError('Invalid Data')
+            return value
+
+    def get_data(self, params=None):
+        return json.loads(self.settings['Data'])
+
+
+class YAMLBackend(alhos_e_bugalhos.connections.Backend):
+    TYPE_NAME = 'YAML'
+    SETTINGS = [
+        'Data',
+    ]
+
+    def validate_setting(self, name: str, value: Any):
+        if name == 'Data':
+            try:
+                dict(yaml.load(value))
+            except (TypeError, ValueError, yaml.YAMLError):
+                raise alhos_e_bugalhos.connections.SettingError('Invalid Data')
+            return value
+
+    def get_data(self, params=None):
+        return dict(yaml.load(self.settings['Data']))
 
 
 class RESTBackend(alhos_e_bugalhos.connections.Backend):
